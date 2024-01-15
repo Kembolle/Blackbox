1e2fd61cce30e9843db971cfb6d3ed0f16532f25
Kembolle Amilkar <Kembolle@users.noreply.github.com>
2024-01-15 17:34:19 -0400

    Arquivo MPMPT

---
+++ geomap_interior.js
@@ -0,0 +1,440 @@
+[
+    {
+        "Key" : "AB",
+        "name" : "Agua Boa",
+        "latitude" : -14.058713395427821,
+        "longitude" : -52.16189369184013
+    },
+    {
+        "Key" : "AF",
+        "name" : "Alta Floresta ",
+        "latitude" : -9.868344398301053,  
+        "longitude" : -56.08050853490562
+    },
+    {
+        "Key" : "AA",
+        "name" : "Alto Araguaia",
+        "latitude" : -17.310913083763815,  
+        "longitude" : -53.194548349918726
+    },
+    {
+        "Key" : "AG",
+        "name" : "Alto Garcas",
+        "latitude" : -16.944957318078696,
+        "longitude" : -53.52990870881903
+    },
+    {
+        "Key" : "AT",
+        "name" : "Alto Taquari",
+        "latitude" : -17.843632944218644,
+        "longitude" : -53.27969883660147
+    },
+    {
+        "Key" : "AP",
+        "name" : "Apiacas",
+        "latitude" : -9.489454929721118,  
+        "longitude" : -57.37224228809739
+    },
+    {
+        "Key" : "AR",
+        "name" : "Araputanga",
+        "latitude" : -15.466618270152322,
+        "longitude" : -58.33044539144168
+    },
+    {
+        "Key" : "AN",
+        "name" : "Arenapolis",
+        "latitude" : -14.456431522299354,  
+        "longitude" : -56.84213806173248
+    },
+    {
+        "Key" : "AI",
+        "name" : "Aripuana ",
+        "latitude" : -10.175818124106993,  
+        "longitude" : -59.45151706667291
+    },
+    {
+        "Key" : "BB",
+        "name" : "Barra do Bugres",
+        "latitude" : -15.06760422861394,  
+        "longitude" : -57.19626214747044
+    },
+    {
+        "Key" : "BG",
+        "name" : "Barra do Garcas",
+        "latitude" : -15.892435136258934,  
+        "longitude" : -52.2618682073912
+    },
+    {
+        "Key" : "BR",
+        "name" : "Brasnorte",
+        "latitude" : -12.121103101218566,  
+        "longitude" : -58.00532658386059
+    },
+    {
+        "Key" : "CC",
+        "name" : "Caceres ",
+        "latitude" : -16.06637264997589,  
+        "longitude" : -57.6847253944652
+    },
+    {
+        "Key" : "CP",
+        "name" : "Campinapolis",
+        "latitude" : -14.542548798952879,  
+        "longitude" : -52.79310434689743
+    },
+    {
+        "Key" : "CN",
+        "name" : "Campo Novo do Parecis",
+        "latitude" : -13.662924594846967,  
+        "longitude" : -57.888314346256685
+    },
+    {
+        "Key" : "CV",
+        "name" : "Campo Verde",
+        "latitude" : -15.54574633778918,  
+        "longitude" : -55.1640231544434
+    },
+    {
+        "Key" : "CR",
+        "name" : "Canarana",
+        "latitude" : -13.587436263006513,
+        "longitude" : -52.26884218503446
+    },
+    {
+        "Key" : "CG",
+        "name" : "Chapada dos Guimaraes",
+        "latitude" : -15.461495973425606,  
+        "longitude" : -55.74611784195254
+    },
+    {
+        "Key" : "CL",
+        "name" : "Claudia",
+        "latitude" : -11.506687907720371,  
+        "longitude" : -54.879482125385415
+    },
+    {
+        "Key" : "CD",
+        "name" : "Colider",
+        "latitude" : -10.803482361138148,  
+        "longitude" : -55.45469666194422
+    },
+    {
+        "Key" : "CZ",
+        "name" : "Colniza",
+        "latitude" : -9.461994063648248,  
+        "longitude" : -59.224034377579805
+    },
+    {
+        "Key" : "CM",
+        "name" : "Comodoro",
+        "latitude" : -13.659721794424438,  
+        "longitude" : -59.79040631869627
+    },
+    {
+        "Key" : "CO",
+        "name" : "Cotriguacu",
+        "latitude" : -9.903233350322225,
+        "longitude" : -58.56596528596221
+    },
+    {
+        "Key" : "DM",
+        "name" : "Diamantino",
+        "latitude" : -14.404973748239383,
+        "longitude" : -56.435711832465316
+    },
+    {
+        "Key" : "FN",
+        "name" : "Feliz Natal",
+        "latitude" : -12.382363827105138,  
+        "longitude" : -54.935981691537656
+    },
+    {
+        "Key" : "GN",
+        "name" : "Guaranta do Norte",
+        "latitude" : -9.950459905195011,  
+        "longitude" : -54.911852937029046
+    },
+    {
+        "Key" : "GR",
+        "name" : "Guiratinga",
+        "latitude" : -16.34527239607475,  
+        "longitude" : -53.75815498373535
+    },
+    {
+        "Key" : "IT",
+        "name" : "Itauba",
+        "latitude" : -11.00714531357586,  
+        "longitude" : -55.24111505441497
+    },
+    {
+        "Key" : "IQ",
+        "name" : "Itiquira",
+        "latitude" : -17.205776830582913,
+        "longitude" : -54.14645793013132
+    },
+    {
+        "Key" : "JC",
+        "name" : "Jaciara",
+        "latitude" : -15.967318316893769,  
+        "longitude" : -54.96706729458356
+    },
+    {
+        "Key" : "JR",
+        "name" : "Jauru",
+        "latitude" : -15.339403478784716,
+        "longitude" : -58.87336797800696
+    },
+    {
+        "Key" : "JU",
+        "name" : "Juara",
+        "latitude" : -11.253654032293325,  
+        "longitude" : -57.50844014462282
+    },
+    {
+        "Key" : "JN",
+        "name" : "Juina",
+        "latitude" : -11.422554278340733,  
+        "longitude" : -58.757523933739364
+    },
+    {
+        "Key" : "LR",
+        "name" : "Lucas do Rio Verde",
+        "latitude" : -11.431854668669565,  
+        "longitude" : -58.77296992329279
+    },
+    {
+        "Key" : "MC",
+        "name" : "Marcelandia",
+        "latitude" : -11.083324697953156,
+        "longitude" : -54.52147831846237
+    },
+    {
+        "Key" : "MT",
+        "name" : "Matupa",
+        "latitude" : -10.169425140357541,  
+        "longitude" : -54.923973885886724
+    },
+    {
+        "Key" : "MD",
+        "name" : "Mirassol D Oeste",
+        "latitude" : -15.676124265689834,  
+        "longitude" : -58.09354943727721
+    },
+    {
+        "Key" : "NB",
+        "name" : "Nobres",
+        "latitude" : -14.718518722821006,  
+        "longitude" : -56.335654427706324
+    },
+    {
+        "Key" : "NL",
+        "name" : "Nortelandia",
+        "latitude" : -14.452954154876263,
+        "longitude" : -56.79919982256571
+    },
+    {
+        "Key" : "NC",
+        "name" : "Nova Canaa do Norte",
+        "latitude" : -10.636940304005355,  
+        "longitude" : -55.709260091532336
+    },
+    {
+        "Key" : "NV",
+        "name" : "Nova Monte Verde",
+        "latitude" : -9.979407831412157,  
+        "longitude" : -57.46752877475687
+    },
+    {
+        "Key" : "NM",
+        "name" : "Nova Mutum",
+        "latitude" : -13.816846130838021,  
+        "longitude" : -56.08727048433183
+    },
+    {
+        "Key" : "NU",
+        "name" : "Nova Ubirata",
+        "latitude" : -13.033130848446243,  
+        "longitude" : -55.25127798606791
+    },
+    {
+        "Key" : "NX",
+        "name" : "Nova Xavantina",
+        "latitude" : -14.675754361368051,
+        "longitude" : -52.35182346429741
+    },
+    {
+        "Key" : "PN",
+        "name" : "Paranaita",
+        "latitude" : -9.666578477644839,  
+        "longitude" : -56.47259419069109
+    },
+    {
+        "Key" : "PG",
+        "name" : "Paranatinga",
+        "latitude" : -14.43099910226838,  
+        "longitude" : -54.049066113932966
+    },
+    {
+        "Key" : "PP",
+        "name" : "Pedra Preta",
+        "latitude" : -16.621500947652564,  
+        "longitude" : -54.48287919730482
+    },
+    {
+        "Key" : "PA",
+        "name" : "Peixoto de Azevedo",
+        "latitude" : -10.242233777447865,  
+        "longitude" : -54.99299491514366
+    },
+    {
+        "Key" : "PC",
+        "name" : "Pocone",
+        "latitude" : -16.264132640761463,
+        "longitude" :  -56.62821531086087
+    },
+    {
+        "Key" : "PL",
+        "name" : "Pontes e Lacerda",
+        "latitude" : -15.231389955624158,  
+        "longitude" : -59.32954610799396
+    },
+    {
+        "Key" : "PD",
+        "name" : "Porto Alegre do Norte",
+        "latitude" : -10.884963243100689,  
+        "longitude" : -51.6393502456195
+    },
+    {
+        "Key" : "PH",
+        "name" : "Porto dos Gauchos",
+        "latitude" : -11.53612877477741,  
+        "longitude" : -57.41105404234051
+    },
+    {
+        "Key" : "PX",
+        "name" : "Poxoreu",
+        "latitude" : -15.826206790788223,
+        "longitude" : -54.402872856350314
+    },
+    {
+        "Key" : "PV",
+        "name" : "Primavera do Leste",
+        "latitude" : -15.557428275818982,
+        "longitude" : -54.296015075792106
+    },
+    {
+        "Key" : "QR",
+        "name" : "Querencia",
+        "latitude" : -12.593384998345533,  
+        "longitude" : -52.205351608961394
+    },
+    {
+        "Key" : "RC",
+        "name" : "Ribeirao Cascalheira",
+        "latitude" : -12.9433812614509,  
+        "longitude" : -51.82644113447964
+    },
+    {
+        "Key" : "RB",
+        "name" : "Rio Branco",
+        "latitude" : -15.240370399162316,  
+        "longitude" : -58.1139657213929
+    },
+    {
+        "Key" : "RD",
+        "name" : "Rondonopolis",
+        "latitude" : -16.456984963315744,  
+        "longitude" : -54.67129593859352
+    },
+    {
+        "Key" : "RO",
+        "name" : "Rosario Oeste",
+        "latitude" : -14.826190450778443,
+        "longitude" : -56.42273661556385
+    },
+    {
+        "Key" : "SA",
+        "name" : "Santo Antonio de Leverger",
+        "latitude" : -15.863754223851231,  
+        "longitude" : -56.077734481774776
+    },
+    {
+        "Key" : "SF",
+        "name" : "Sao Felix do Araguaia",
+        "latitude" : -11.618176317186775,
+        "longitude" :  -50.66510240814835
+    },
+    {
+        "Key" : "SJ",
+        "name" : "Sao Jose do Rio Claro",
+        "latitude" : -13.445873766749592,
+        "longitude" : -56.71291348732556
+    },
+    {
+        "Key" : "SQ",
+        "name" : "Sao Jose dos Quatro Marcos",
+        "latitude" : -15.565152209871675,  
+        "longitude" : -58.19067545065595
+    },
+    {
+        "Key" : "SP",
+        "name" : "Sapezal",
+        "latitude" : -13.547975451504474,
+        "longitude" : -58.8169609607546
+    },
+    {
+        "Key" : "SI",
+        "name" : "Sinop",
+        "latitude" : -11.853962874666195,  
+        "longitude" : -55.50964305958484
+    },
+    {
+        "Key" : "SR",
+        "name" : "Sorriso",
+        "latitude" : -12.540567437055284,
+        "longitude" : -55.72418725684351
+    },
+    {
+        "Key" : "TB",
+        "name" : "Tabapora",
+        "latitude" : -11.308278091012953,
+        "longitude" : -56.82070178351073
+    },
+    {
+        "Key" : "TS",
+        "name" : "Tangara da Serra",
+        "latitude" : -14.618345030244278,
+        "longitude" : -57.48691288915858
+    },
+    {
+        "Key" : "TP",
+        "name" : "Tapurah",
+        "latitude" : -12.737961187353424,
+        "longitude" : -56.514450469744794
+    },
+    {
+        "Key" : "TN",
+        "name" : "Terra Nova do Norte",
+        "latitude" : -10.598950913969126,  
+        "longitude" : -55.11744979100384
+    },
+    {
+        "Key" : "VA",
+        "name" : "Vera",
+        "latitude" : -12.286856872794264,
+        "longitude" : -55.29707562278499
+    },
+    {
+        "Key" : "VB",
+        "name" : "Vila Bela da Santissima Trindade",
+        "latitude" : -15.005164627012318,
+        "longitude" : -59.94783866662135
+    },
+    {
+        "Key" : "VR",
+        "name" : "Vila Rica",
+        "latitude" : -10.01367496061083,
+        "longitude" : -51.1172671320469
+    },
+]
